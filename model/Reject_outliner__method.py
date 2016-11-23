110e04d9a130b857bf87ad984381e3411425b2a8
Xiao <lingxiao1989@gmail.com>
2016-11-23 15:41:55 -0800

    Update Reject_outliner__method.py

--- model/Reject_outliner__method.py
+++ model/Reject_outliner__method.py
@@ -1,7 +1,6 @@
 import cv2,numpy as np
 import matplotlib.pyplot as plt
 import time
-#from PIL import Image
 
 def reject_outliers(index,frames):
 	data=[]
@@ -18,7 +17,6 @@ def average(index,frames):
 	return np.mean(result)
 		
 np.set_printoptions(threshold=np.nan)
-#f1=open('./testfile', 'w+')
 vidcap = cv2.VideoCapture('My Movie.mp4')
 frames=[]
 count = 0
@@ -36,26 +34,17 @@ i = 0
 diffcount=np.zeros(frames[0].shape)
 
 while (i<count):
-#cv2.imwrite("frame%d.jpg" % count, frames[])     # save frame as JPEG file
-#cv2.imwrite("frame3.jpg", frames[1])     # save frame as JPEG file
 	pre=np.array(frames[i])
 	i+=1
 	j=i
 	while(j<count):
-	#height, width = U.shape
-	#print height, width
-	#np.savetxt("./testfile%d" % count,U)
 		diff= np.array(frames[j]-frames[i-1])
 		diff[diff>205]=0
 		diff[diff<50]=0
 		diff[diff!=0]=1
 		diffcount+=diff
 		j += 1
-		#framesdiff.append(diff)
-#print >>f1, frames[1]
-#U1, sigma1, V1 = np.linalg.svd(frames[0])
-#U2, sigma2, V2 = np.linalg.svd(frames[1])
-#U3, sigma3, V3 = np.linalg.svd(frames[2])
+
 background= np.array(frames[0])
 for index, x in np.ndenumerate(diffcount):
 	if x>0:
@@ -64,15 +53,4 @@ for index, x in np.ndenumerate(diffcount):
 		background[index]=average(index, frames)
 		
 
-#a= np.array(frames[2]-frames[1])
-#a[a>205]=0
-#a[a<50]=0
-#a[a!=0]=255
-#np.savetxt("./testfile",diffcount)
 cv2.imwrite("reconstimg.jpg",background)
-#U, sigma, V = np.linalg.svd(image)	
-#print np.mean(test)
-#background= np.array(frames[0])
-#mean=background.flatten()
-#cov=np.eye(mean.size)
-#reconstimg=np.matrix(U3[:,:240]) * np.diag(sigma1) * np.matrix(V3[:240,:])
