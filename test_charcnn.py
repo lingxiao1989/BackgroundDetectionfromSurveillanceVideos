03d891d92f7bafbbc6c13202522c53bb058ffb14
Xiao <lingxiao1989@gmail.com>
2020-10-07 11:57:26 +0800

    update

--- test_charcnn.py
+++ test_charcnn.py
@@ -1,15 +1,10 @@
-# set up logging
-import logging
-logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
-        datefmt="%m/%d/%Y %H:%M:%S",
-        level=logging.INFO,
-)
-
-
-# make deterministic
-from mingpt.utils import set_seed
-set_seed(42)
+import random
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.nn import functional as F
+from model.minGTP import GPT, GPTConfig
+from model.trainer import Trainer, TrainerConfig
 
 import numpy as np
 import torch
@@ -18,6 +13,16 @@ from torch.nn import functional as F
 
 import math
 from torch.utils.data import Dataset
+
+import logging
+#from mingpt.utils import set_seed
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
 
 class CharDataset(Dataset):
 
@@ -77,31 +82,76 @@ class CharDataset(Dataset):
         y = torch.tensor(dix[1:], dtype=torch.long)
         return x, y
 
-block_size = 128 # spatial extent of the model for its context
-
-# you can download this file at https://github.com/karpathy/char-rnn/blob/master/data/tinyshakespeare/input.txt
-text = open('input.txt', 'r').read() # don't worry we won't run out of file handles
-train_dataset = CharDataset(text, block_size) # one line of poem is roughly 50 characters
-
-from mingpt.model import GPT, GPTConfig
-mconf = GPTConfig(train_dataset.vocab_size, train_dataset.block_size,
-                  n_layer=8, n_head=8, n_embd=512)
-model = GPT(mconf)
-
-from mingpt.trainer import Trainer, TrainerConfig
-
-# initialize a trainer instance and kick off training
-tconf = TrainerConfig(max_epochs=2, batch_size=512, learning_rate=6e-4,
-                      lr_decay=True, warmup_tokens=512*20, final_tokens=2*len(train_dataset)*block_size,
-                      num_workers=4)
-trainer = Trainer(model, train_dataset, None, tconf)
-trainer.train()
 
 # alright, let's sample some character-level Shakespeare
-from mingpt.utils import sample
+#from mingpt.utils import sample
 
-context = "O God, O God!"
-x = torch.tensor([train_dataset.stoi[s] for s in context], dtype=torch.long)[None,...].to(trainer.device)
-y = sample(model, x, 2000, temperature=1.0, sample=True, top_k=10)[0]
-completion = ''.join([train_dataset.itos[int(i)] for i in y])
-print(completion)
+def top_k_logits(logits, k):
+    v, ix = torch.topk(logits, k)
+    out = logits.clone()
+    out[out < v[:, [-1]]] = -float('Inf')
+    return out
+
+@torch.no_grad()
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+    """
+    take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
+    the sequence, feeding the predictions back into the model each time. Clearly the sampling
+    has quadratic complexity unlike an RNN that is only linear, and has a finite context window
+    of block_size, unlike an RNN that has an infinite context window.
+    """
+    block_size = model.get_block_size()
+    model.eval()
+    for k in range(steps):
+        x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
+        logits, _ = model(x_cond)
+        # pluck the logits at the final step and scale by temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop probabilities to only the top k options
+        if top_k is not None:
+            logits = top_k_logits(logits, top_k)
+        # apply softmax to convert to probabilities
+        probs = F.softmax(logits, dim=-1)
+        # sample from the distribution or take the most likely
+        if sample:
+            ix = torch.multinomial(probs, num_samples=1)
+        else:
+            _, ix = torch.topk(probs, k=1, dim=-1)
+        # append to the sequence and continue
+        x = torch.cat((x, ix), dim=1)
+    
+    return x
+
+if __name__ == '__main__':
+    # set up logging
+    logging.basicConfig(
+            format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+            datefmt="%m/%d/%Y %H:%M:%S",
+            level=logging.INFO,
+    )
+    # make deterministic
+    set_seed(42)
+
+    block_size = 128 # spatial extent of the model for its context
+
+    # you can download this file at https://github.com/karpathy/char-rnn/blob/master/data/tinyshakespeare/input.txt
+    text = open('data/input.txt', 'r').read() # don't worry we won't run out of file handles
+    train_dataset = CharDataset(text, block_size) # one line of poem is roughly 50 characters
+
+    mconf = GPTConfig(train_dataset.vocab_size, train_dataset.block_size,
+                    n_layer=8, n_head=8, n_embd=512)
+    model = GPT(mconf)
+
+
+    # initialize a trainer instance and kick off training
+    tconf = TrainerConfig(max_epochs=2, batch_size=512, learning_rate=6e-4,
+                        lr_decay=True, warmup_tokens=512*20, final_tokens=2*len(train_dataset)*block_size,
+                        num_workers=4)
+    trainer = Trainer(model, train_dataset, None, tconf)
+    trainer.train()
+
+    context = "O God, O God!"
+    x = torch.tensor([train_dataset.stoi[s] for s in context], dtype=torch.long)[None,...].to(trainer.device)
+    y = sample(model, x, 2000, temperature=1.0, sample=True, top_k=10)[0]
+    completion = ''.join([train_dataset.itos[int(i)] for i in y])
+    print(completion)
